01b8b471eec9105f21421750fa91f2553ab265f7
Ishra24338 <nadiraema48829@gmail.com>
2026-09-04 12:01:23 +0600

    Add files via upload

---
+++ print.py
@@ -0,0 +1,10 @@
+# print the first python code
+print("Hello World!!")
+# printing my name
+print("Ishra")
+# printing a fun fact about myself
+print("I love playing Basketball")
+#let me print my age
+print("My age is",12)
+#addition of 3 and 5
+print("The addition of 3 and 5 is",3+5)
