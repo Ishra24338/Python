6276c44b6d6fd0bdf2e98be9881bdc02bb6fedc8
Ishra24338 <nadiraema48829@gmail.com>
2026-09-11 21:15:59 +0600

    Add files via upload

---
+++ my_daily_routine.py
@@ -0,0 +1,16 @@
+# Printing the Title
+print("My Daily Routine")     
+# Step 1, waking up
+print("I wake up at 7 a.m.")
+#step 2, breakfast
+print("I have breakfast at 7:30.")
+#step 3, going to school
+print("I go to  school at 8 a.m.")
+#step 4, coming home
+print("I come home at 1 p.m.")
+#step 5, lunch
+print("I have lunch at 2 p.m.")
+#Step 6, studying
+print("I study for 2 hours in the evening")
+#Calculate total weekly study time
+print("Total study hours in a week", 2*7)
