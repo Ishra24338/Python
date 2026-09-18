c734b346e9cc204b3a86f19914309f6e5edaeb3f
Ishra24338 <nadiraema48829@gmail.com>
2026-09-18 12:13:09 +0600

    Add files via upload

---
+++ input.py
@@ -0,0 +1,12 @@
+#creating different variables
+x = 5
+y = "John" 
+print(x)
+print(y)
+
+codingal="codingal"
+print(codingal)
+
+#user input
+name = input("enter your name:  ")
+print("\n Hello",name, "\nWelcome to codingal")
---
+++ printing.py
@@ -0,0 +1,10 @@
+#print a sentence
+print("Welcome to the world of programming.")
+#print a number
+print(4)
+#next line after print
+print("Hello python \n")
+#pass multiple arguements for printing
+print("hello",5)
+#End arguement of print statement
+print("Welcome to ", end="*")
